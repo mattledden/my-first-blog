966ae31e9925b566e4e47307ceda6d95e201ed19
Matt <matt.ledden@gmail.com>
2020-06-01 16:04:50 +0100

    started unit testing

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,19 @@
+from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+from blog.views import post_list
 
-# Create your tests here.
+class PostListTest(TestCase):
+
+    def test_root_url_resolves_to_post_list_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, post_list)
+
+    
+    def test_post_list_returns_correct_html(self):
+        request = HttpRequest()
+        response = post_list(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('\n<html>'))
+        self.assertIn("<title>Matt's blog</title>", html)
+        self.assertTrue(html.endswith('</html>'))
