cf66eee01246dceae44aee47ed69b4727b362460
Matt <matt.ledden@gmail.com>
2020-06-03 16:12:50 +0100

    Refactoring

--- Testing/functional_tests.py
+++ Testing/functional_tests.py
@@ -1,8 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
 
-browser = webdriver.Firefox(executable_path='C:/Users/Matt/bridge/Testing/geckodriver.exe')
-browser.get('http://127.0.0.1:8000/')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'blog' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox(executable_path='C:/Users/Matt/bridge/Testing/geckodriver.exe')
 
-browser.quit()
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online blog. She goes
+        # to check out its homepage
+        self.browser.get('http://127.0.0.1:8000')
+
+        # She notices the page title and header mention a blog
+        self.assertIn('blog', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text  
+        self.assertIn('Blog', header_text)
+
+if __name__ == '__main__':  
+    unittest.main(warnings='ignore')
--- blog/tests.py
+++ blog/tests.py
@@ -4,16 +4,9 @@ from django.http import HttpRequest
 from blog.views import post_list
 
 class PostListTest(TestCase):
-
-    def test_root_url_resolves_to_post_list_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, post_list)
-
     
     def test_post_list_returns_correct_html(self):
-        request = HttpRequest()
-        response = post_list(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('\n<html>'))
-        self.assertIn("<title>Matt's blog</title>", html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'blog/post_list.html', 'blog/base.html')
+
+    
