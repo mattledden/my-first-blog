ef8610d1f9dd08a04c74dc265e69e49f0756aecc
Matt <matt.ledden@gmail.com>
2020-06-16 15:04:41 +0100

    Tests for adding new cv work when password requirement is lifted or password is given

--- Testing/functional_tests.py
+++ Testing/functional_tests.py
@@ -1,3 +1,6 @@
+#DISCLAIMER!
+#these tests work when I allow the real password to be inputted but obviously I didn't want that to be uploaded to GitHub!!
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
@@ -16,28 +19,119 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_make_cv(self):
         # Edith has heard about a cool new online blog. She goes
         # to check out its homepage
-        self.browser.get('http://127.0.0.1:8000/cv/new')
+        self.browser.get('http://127.0.0.1:8000/accounts/login/?next=/cv/new')
+
+        #She needs to login
+        inputbox = self.browser.find_element_by_id('id_username')
+        
+        inputbox.send_keys('USERNAME')
+        inputbox.send_keys(Keys.TAB)
+
+        inputbox = self.browser.find_element_by_id('id_password')
+        
+        inputbox.send_keys('PASSWORD') #I haven't put the actual password here as I don't want to upload it to GitHub!! 
+        inputbox.send_keys(Keys.ENTER)
+
+        time.sleep(1)
 
         # She notices the page title and header mention a blog
         self.assertIn('Blog', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text  
+        header_text = self.browser.find_element_by_tag_name('h2').text  
         self.assertIn('CV', header_text)
         
-        #She is invited to enter qualifications
-        inputbox = self.browser.find_element_by_id('id_new_qual')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a qualification'
-        )
+        #She is invited to enter her name
+        inputbox = self.browser.find_element_by_id('id_name')
+        
+        # She types "Edith"
+        inputbox.send_keys('Edith')
+        
+        # When she hits tab she can fill in the next field
+        inputbox.send_keys(Keys.TAB)
+
+        #She is invited to enter her location
+        inputbox = self.browser.find_element_by_id('id_location')
+        
+        # She types "Birmingham"
+        inputbox.send_keys('Birmingham')
+        
+        # When she hits tab she can fill in the next field
+        inputbox.send_keys(Keys.TAB)
+
+        #She is invited to enter her age
+        inputbox = self.browser.find_element_by_id('id_age')
+        
+        # She types "22"
+        inputbox.send_keys('22')
+        
+        # When she hits tab she can fill in the next field
+        inputbox.send_keys(Keys.TAB)
+
+        #She is invited to enter her contact details
+        inputbox = self.browser.find_element_by_id('id_contact')
+        
+        # She types "999, edith@gmail.com"
+        inputbox.send_keys('999, edith@gmail.com')
+        
+        # When she hits tab she can fill in the next field
+        inputbox.send_keys(Keys.TAB)
+
+        #She is invited to enter a profile
+        inputbox = self.browser.find_element_by_id('id_profile')
+        
+        # She types "This is my profile"
+        inputbox.send_keys('This is my profile')
+        
+        # When she hits tab she can fill in the next field
+        inputbox.send_keys(Keys.TAB)
+
+        #She is invited to enter her experience
+        inputbox = self.browser.find_element_by_id('id_experience')
+        
+        # She types "lots"
+        inputbox.send_keys('lots')
+        
+        # When she hits tab she can fill in the next field
+        inputbox.send_keys(Keys.TAB)
+
+        #She is invited to enter her qualifications
+        inputbox = self.browser.find_element_by_id('id_qualifications')
+        
         # She types "GCSE Maths Grade A"
         inputbox.send_keys('GCSE Maths Grade A')
         
-        # When she hits enter, the page updates, and now the page lists
-        # "GCSE Maths Grade A" as a qualification
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        # When she hits tab she can fill in the next field
+        inputbox.send_keys(Keys.TAB)
 
-    def test_can_read_cv(self):
+        #She is invited to enter her hobbies
+        inputbox = self.browser.find_element_by_id('id_hobbies')
+        
+        # She types "Singing"
+        inputbox.send_keys('Singing')
+        
+        # When she hits tab she can fill in the next field
+        inputbox.send_keys(Keys.TAB)
+
+        #She is invited to enter her references
+        inputbox = self.browser.find_element_by_id('id_references')
+        
+        # She types "on request"
+        inputbox.send_keys('On request')
+
+        time.sleep(1)
+        
+        # When she hits tab she can fill in the next field
+        #inputbox.send_keys(Keys.TAB)
+
+        #time.sleep(1)
+        inputbox = self.browser.find_element_by_id('id_submit')
+        time.sleep(1)
+        #when she hits enter the form is submitted and she is redirected to the page displaying her CV
+        inputbox.send_keys(Keys.ENTER)
+
+        time.sleep(2)
+        
+
+    '''def test_can_read_cv(self):
         # Edith has heard about a cool new online blog. She goes
         # to check out its homepage
         self.browser.get('http://127.0.0.1:8000/cv')
@@ -58,6 +152,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('CV', header_text)
 
         #edit stuff
+        '''
 
 if __name__ == '__main__':  
     unittest.main(warnings='ignore')
--- blog/tests.py
+++ blog/tests.py
@@ -1,34 +1,42 @@
+#DISCLAIMER
+#these tests work when I comment out the lines saying @login_required in the views.py file
+#I have uncommented those lines though as I want the edit and create CV features to only be available to me
+
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from blog.views import post_list
 from blog.models import CV
+import time
 
-class PostListTest(TestCase):
+class CVListTest(TestCase):
+
+    def test_returns_correct_html(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'blog/post_list.html', 'blog/base.html')
     
-    def test_cv_returns_correct_html(self):
+    '''def test_cv_returns_correct_html(self):
         response = self.client.get('/cv/')
         self.assertTemplateUsed(response, 'cv/cv.html', 'blog/base.html')
 
     def test_edit_cv_returns_correct_html(self):
         response = self.client.get('/cv/edit/')
-        self.assertTemplateUsed(response, 'cv/edit_cv.html', 'blog/base.html')
+        self.assertTemplateUsed(response, 'cv/cv_edit.html', 'blog/base.html')'''
 
     def test_new_cv_returns_correct_html(self):
-        response = self.client.get('/cv/new/')
-        self.assertTemplateUsed(response, 'cv/edit_cv.html', 'blog/base.html')
+        response2 = self.client.get('/cv/new/')
+        self.assertTemplateUsed(response2, 'cv/cv_edit.html', 'blog/base.html')
 
     def test_can_save_a_POST_request(self):
-        self.client.post('/cv/new/', data={'name': 'Matt'})
-
+        self.client.post('/cv/new/', data={'name': 'Matt', 'location': 'a', 'age': '1', 'contact': 'a', 'profile': 'a', 'experience': 'a', 'qualifications': 'a', 'hobbies': 'a', 'references': 'a'})
         self.assertEqual(CV.objects.count(), 1)
         new_cv = CV.objects.first()
         self.assertEqual(new_cv.name, 'Matt')
 
     def test_redirects_after_POST(self):
-        response = self.client.post('/cv/new/', data={'name': 'Matt'})
+        response = self.client.post('/cv/new/', data={'name': 'Matt', 'location': 'a', 'age': '1', 'contact': 'a', 'profile': 'a', 'experience': 'a', 'qualifications': 'a', 'hobbies': 'a', 'references': 'a'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/cv/')
+        self.assertEqual(response['location'], '/cv/1/')
 
     def test_only_saves_cv_when_necessary(self):
         self.client.get('/cv/')
